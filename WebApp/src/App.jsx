b8fc9b7cdd9ae088106f6dcf178f85e2c9f1d537
mohamedmaghzaoui <mohamedmaghzaoui53@gmail.com>
2025-04-07 15:15:51 +0200

    added react routes

--- WebApp/src/App.jsx
+++ WebApp/src/App.jsx
@@ -1,31 +1,22 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar.jsx'
+// App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './App.css';
+import Navbar from './components/Navbar.jsx';
+import {Home} from "./pages/home/home.jsx"
 
 function App() {
   return (
-    <>
-    <Navbar/>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Navbar />  
+      {/* Define Routes here */}
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Home component for the home page */}
+      </Routes>
+
+    
+    </Router>
+  );
 }
 
 export default App;
--- WebApp/src/components/Navbar.jsx
+++ WebApp/src/components/Navbar.jsx
@@ -1,27 +1,30 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-
+import { Link } from 'react-router-dom';
 export default function Navbar() {
     return (
         <>
-            <nav class="navbar bg-body-tertiary fixed-top">
-                <div class="container-fluid">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar bg-body-tertiary fixed-top">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                        <div class="offcanvas-header">
-                            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">RecognizeMe</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    <div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">RecognizeMe</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
-                        <div class="offcanvas-body">
-                            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                <li class="nav-item">
-                                    <a class="nav-link" aria-current="page" href="/classes">Classes</a>
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li className="nav-item">
+                                <Link to={"/classes"}>Classes</Link>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" aria-current="page" href="/etudiants">Étudiants</a>
+                                <li className="nav-item">
+                                <Link to={"/students"}>Etudiants</Link>
                                 </li>
+                                
+                                <button className='btn btn-primary my-4'>Connexion</button>
+                                <button className='btn btn-success'>Inscription</button>
                             </ul>
                         </div>
                     </div>
