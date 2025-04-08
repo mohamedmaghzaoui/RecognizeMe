e7a3bdbdc61ab22ec9bb78ab6aba7a0f649ce005
Nino Berber <95708793+nin0dev@users.noreply.github.com>
2025-04-08 11:21:15 +0200

    Add table component

---
+++ WebApp/src/components/Table.jsx
@@ -0,0 +1,32 @@
+export const Table = () => {
+    return (
+        <div>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Nom de la classe</th>
+                        <th scope="col">Nombre d'apprenants</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">Chris</th>
+                        <td>HTML tables</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Dennis</th>
+                        <td>Web accessibility</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Sarah</th>
+                        <td>JavaScript frameworks</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Karen</th>
+                        <td>Web performance</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
--- WebApp/src/pages/profile/profile.jsx
+++ WebApp/src/pages/profile/profile.jsx
@@ -1,8 +1,6 @@
-export const Profile=()=>{
+export const Profile = () => {
     return (
         <div>
-        
-        
             
         </div>
     )
